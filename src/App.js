1b34a3ea5036b2195ab26ccb7e0d805ad40758c9
Aadhil Ahamed <aadhil2k4@gmail.com>
2024-07-13 23:29:30 +0530

    navbar issue

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Table from './components/Table';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Table />} />
       </Routes>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div>
         <nav className="navbar navbar-light bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand text-light" href="/">OS-Scheduling Algorithms</a>
+    <a className="navbar-brand text-light" href="https://aadhil2k4.github.io/Process_Scheduling_Calculator/">OS-Scheduling Algorithms</a>
     <div className='d-flex'><a href="https://github.com/aadhil2k4/Process_Scheduling_Calculator"><i style={{color: 'white', fontSize: '2rem'}} class="uil uil-github"></i></a></div>
   </div>
 </nav>
